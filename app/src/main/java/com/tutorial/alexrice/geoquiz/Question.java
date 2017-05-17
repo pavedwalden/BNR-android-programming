f1bd2dd61b56b4157f9de2d0c0dd60cc5fad6cd0
Alex Rice <rice.alexanderb@gmail.com>
2017-05-17 16:35:27 -0700

    saving changes before removing and re-installing Android Studio

---
+++ app/src/main/java/com/tutorial/alexrice/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.tutorial.alexrice.geoquiz;
+
+/**
+ * Created by alexrice on 5/17/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
